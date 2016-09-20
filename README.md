843dc4758366e6ea60de0482e0130f4ee5c7854f
Karlo Espiritu <karloespiritu@gmail.com>
2016-09-21 00:14:40 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# BEMSkel
+
+BEMSkel is a lightweight responsive CSS framework using BEM and SASS which includes the
+best practices and important UI components that I use on every web and React projects.
+
+BEMSkel is designed to be a starting point. A solid foundation for your project, leaving the creativity up to you.
+
+- Minified production versions: [`dist/`](dist/)
+- Unminified sources: [`src/`](src/)
+
+## Features
+
+- Written in BEM (Block, Modifier, Modifier) naming convention for classes to provide a
+consistent and strict to easily extend the framework.
+- Uses <a href="https://sass-guidelin.es/#architecture">7-1 Architecture pattern</a> for maintainable and scalable SASS.
+- Includes two methods of creating grids - CSS Grid and Flexbox
+- Gulp Build System
+- Designed to be scalable and easy to customize. Use it as a standalone library or use it a starting point to build your next application.
+
+## Quick Start
+
+```
+$ git clone git@github.com:karloespiritu/bemskel.git
+```
+
+
+## Credits
+
+- Normalize.css (http://necolas.github.io/normalize.css | Nicolas Gallagher, Jonathan Neal | MIT License)
+- SASS Boilerplate (https://github.com/HugoGiraudel/sass-boilerplate) | Hugo Giraudel | MIT License)
+- Skeleton (http://getskeleton.com) | Dave Gamache | MIT License)
---
+++ docs/js/site.js
@@ -0,0 +1,94 @@
+"use strict";
+
+$(document).ready(function() {
+
+  // Variables
+  var $codeSnippets = $('.code-sample__body'),
+      $nav = $('.navbar'),
+      $body = $('body'),
+      $window = $(window),
+      $popoverLink = $('[data-popover]'),
+      navOffsetTop = $nav.offset().top,
+      $document = $(document),
+      entityMap = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': '&quot;',
+        "'": '&#39;',
+        "/": '&#x2F;'
+      }
+
+  function init() {
+    $window.on('scroll', onScroll)
+    $window.on('resize', resize)
+    $popoverLink.on('click', openPopover)
+    $document.on('click', closePopover)
+    $('a[href^="#"]').on('click', function(e) {
+      e.preventDefault();
+      smoothScroll($(window), $($(e.currentTarget).attr('href')).offset().top, 700);
+    })
+    renderCodeSnippets();
+  }
+
+  /* usage */
+  function smoothScroll(el, to, duration) {
+      if (duration < 0) {
+          return;
+      }
+      var difference = to - $(window).scrollTop() - 40;
+      var perTick = difference / duration * 10;
+      var scrollToTimerCache = setTimeout(function() {
+          if (!isNaN(parseInt(perTick, 10))) {
+            window.scrollTo(0, $(window).scrollTop() + perTick);
+            smoothScroll(el, to, duration - 10);
+          }
+      }.bind(this), 10);
+  }
+
+
+  function openPopover(e) {
+    e.preventDefault()
+    closePopover();
+    var popover = $($(this).data('popover'));
+    popover.toggleClass('open')
+    e.stopImmediatePropagation();
+  }
+
+  function closePopover(e) {
+    if($('.popover.open').length > 0) {
+      $('.popover').removeClass('open')
+    }
+  }
+
+  function resize() {
+    $body.removeClass('has-docked-nav')
+    navOffsetTop = $nav.offset().top
+    onScroll()
+  }
+
+  function onScroll() {
+    if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
+      $body.addClass('has-docked-nav')
+    }
+    if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
+      $body.removeClass('has-docked-nav')
+    }
+  }
+
+  function escapeHtml(string) {
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+      return entityMap[s];
+    });
+  }
+
+  function renderCodeSnippets() {
+    $codeSnippets.each(function() {
+      var newContent = escapeHtml($(this).html())
+      $(this).html(newContent)
+    })
+  }
+
+  init();
+
+});
